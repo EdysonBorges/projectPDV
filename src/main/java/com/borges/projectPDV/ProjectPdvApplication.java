2f0dd1d668f3781776e5ee51bd64b2079484414c
EdysonBorges <Edysonborges@gmail.com>
2025-02-22 09:55:27 -0300

    List Categoria com method GET

---
+++ src/main/java/com/borges/projectPDV/ProjectPdvApplication.java
@@ -0,0 +1,15 @@
+package com.borges.projectPDV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPdvApplication.class, args);
+	}
+	
+	
+
+}
---
+++ src/test/java/com/borges/projectPDV/ProjectPdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borges.projectPDV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
